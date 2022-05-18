530cad2d7e65f008de314466bc47a312e9a3baf7
GentaArya <mgentaarya@yahoo.com>
2022-05-18 15:20:24 +0700

    menambahkan login dan register

---
+++ app/src/main/java/com/wastecreative/wastecreative/presentation/view/login/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.wastecreative.wastecreative.presentation.view.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wastecreative.wastecreative.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
