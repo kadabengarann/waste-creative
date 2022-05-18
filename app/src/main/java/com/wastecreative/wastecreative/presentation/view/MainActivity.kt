a25cf3e02a09b102e7801d0489db60c71803a571
Ardhy Satrio <rio27dh@gmail.com>
2022-05-18 16:30:13 +0800

    test commit

--- app/src/main/java/com/wastecreative/wastecreative/presentation/view/MainActivity.kt
+++ app/src/main/java/com/wastecreative/wastecreative/presentation/view/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                R.id.navigation_home,R.id.navigation_marketplace
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
 
