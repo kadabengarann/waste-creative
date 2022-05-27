e2af187b4c99fadf62282fba1f8b9d0cd9388b3c
Ardhy Satrio <rio27dh@gmail.com>
2022-05-27 13:07:01 +0800

    Marketplace page

--- app/src/main/java/com/wastecreative/wastecreative/presentation/view/marketplace/MarketplaceFragment.kt
+++ app/src/main/java/com/wastecreative/wastecreative/presentation/view/marketplace/MarketplaceFragment.kt
@@ -1,24 +1,77 @@
 package com.wastecreative.wastecreative.presentation.view.marketplace
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.wastecreative.wastecreative.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wastecreative.wastecreative.data.models.Post
+import com.wastecreative.wastecreative.databinding.FragmentMarketplaceBinding
+import com.wastecreative.wastecreative.presentation.adapter.PostListAdapter
 
 class MarketplaceFragment : Fragment() {
+    private var _binding: FragmentMarketplaceBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val postListAdapter: PostListAdapter by lazy {
+        PostListAdapter(
+            arrayListOf()
+        )
     }
+    private val data = ArrayList<Post>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_marketplace, container, false)
+    ): View {
+        _binding = FragmentMarketplaceBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        makeDummyData()
+        showRecyclerList()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.fabCreatePost.setOnClickListener {
+//            view?.findNavController()?.navigate(R.id.action_navigation_craft_to_navigation_add_detail_craft)
+        }
+    }
+
+    private fun makeDummyData() {
+        if (data.isEmpty()) {
+            for (i in 1..20) {
+                val items = Post(
+                    i.toString(),
+                    "Saifuddin",
+                    "https://picsum.photos/300/300?random=$i",
+                    "Dijual cepat Kapal Mainan $i",
+                    "https://picsum.photos/200/300?random=$i",
+                    "yoman $i"
+                )
+                data.add(items)
+            }
+        }
+    }
+
+    private fun showRecyclerList() {
+        binding.rvCrafts.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = postListAdapter
+        }
+        postListAdapter.setData(data)
+        postListAdapter.setOnItemClickCallback(object : PostListAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Post) {
+                /*val intentToDetail = Intent(requireActivity(), DetailCraftActivity::class.java)
+                intentToDetail.putExtra(DetailCraftActivity.EXTRA_CRAFT, data)
+                startActivity(intentToDetail)*/
+            }
+        })
+    }
 }
